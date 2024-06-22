eac57026bbcc3cd1f1d26bfdac64229a7ad40390
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2024-06-22 19:07:01 +0100

    feat: setup zsm project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "light-green": "#EFFFE1",
+        green: "#4EA507",
+        grey: {
+          dark: {
+            1: "#021C2F",
+            2: "#354959",
+            3: "#808D97",
+            4: "#F2F4F5"
+          }
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
